c427f7681c770490d152cca555ce0274e72e1202
baihaotian <haotian_bai@kingdee.com>
2021-10-25 23:13:45 +0800

    .

--- JavaTest/src/gy/test/C.java
+++
@@ -1,8 +0,0 @@
-package gy.test;
-
-/**
- * @author ：BHT
- * @date ：2019-11-07 10:37
- */
-public class C extends F {
-}
--- JavaTest/src/utils/spider/Video.java
+++
@@ -1,8 +0,0 @@
-package utils.spider;
-
-/**
- * @author ：BHT
- * @date ：2019-09-07 09:28
- */
-public class Video {
-}
